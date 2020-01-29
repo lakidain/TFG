9d7cb7259552fa2271dbe0c2cc24e0f7f13e6260
Ander Lakidain de Arriba <lakidainander@gmail.com>
2020-01-29 14:14:36 +0100

    Update database_architecture.sql

--- Resources/database_architecture.sql
+++ Resources/database_architecture.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-01-2020 a las 03:10:07
+-- Tiempo de generación: 29-01-2020 a las 14:14:13
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.10
 
@@ -35,15 +35,42 @@ CREATE TABLE `asi_appointment` (
   `id_audit` int(11) NOT NULL,
   `name_appointment` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `description_appointment` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
-  `date_appointment` datetime NOT NULL
+  `date_appointment` datetime NOT NULL,
+  `state_appointment` int(11) NOT NULL COMMENT '0 Open and 1 Close'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `asi_appointment`
 --
 
-INSERT INTO `asi_appointment` (`id_appointment`, `id_audit`, `name_appointment`, `description_appointment`, `date_appointment`) VALUES
-(2, 1, 'Coches wapos', 'Amos a ver los cochecillos', '2020-01-28 23:50:00');
+INSERT INTO `asi_appointment` (`id_appointment`, `id_audit`, `name_appointment`, `description_appointment`, `date_appointment`, `state_appointment`) VALUES
+(2, 1, 'Coches wapos', 'Amos a ver los cochecillos', '2020-01-28 23:50:00', 0),
+(3, 1, 'Cableado Electrico', 'Nos encargaremos de hablar sobre el cableado electrico', '2020-01-31 14:00:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asi_appointments_gallery`
+--
+
+CREATE TABLE `asi_appointments_gallery` (
+  `id_gallery` int(11) NOT NULL,
+  `id_appointment` int(11) NOT NULL,
+  `photo_gallery` varchar(200) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `asi_appointments_gallery`
+--
+
+INSERT INTO `asi_appointments_gallery` (`id_gallery`, `id_appointment`, `photo_gallery`) VALUES
+(1, 2, '31247.jpg'),
+(2, 2, '47741453-189c-4f4f-a6cb-57eb291d152f_31247.jpg'),
+(3, 2, '4f7ba53a-ad70-4b02-9f54-aeb4d68dc65f_31247.jpg'),
+(4, 2, 'a08039d5-a6af-4273-9f66-b2fee0daa57b_31247.jpg'),
+(5, 2, '63fd0881-5adb-432f-bb01-9f2a13e25773_31247.jpg'),
+(6, 2, '5b43ee4e-94ca-4ddc-af39-8015b6ac2e1d_amazon-reveals-new-server-chip-to-take-on-intel-1280x720.jpg'),
+(7, 3, 'f14d88b6-c399-444b-9381-445dd46daced_Añadirunpocodetexto(4).png');
 
 -- --------------------------------------------------------
 
@@ -235,6 +262,30 @@ INSERT INTO `asi_companies` (`id_company`, `name_company`, `cif_company`, `busin
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `asi_messages`
+--
+
+CREATE TABLE `asi_messages` (
+  `id_message` int(11) NOT NULL,
+  `id_appointment` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `text_message` varchar(2000) COLLATE utf8_spanish_ci NOT NULL,
+  `timestamp_message` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `asi_messages`
+--
+
+INSERT INTO `asi_messages` (`id_message`, `id_appointment`, `id_user`, `text_message`, `timestamp_message`) VALUES
+(1, 2, 7, 'Morbi dignissim nec odio nec dictum. Cras elementum, massa eget luctus viverra, erat ipsum rutrum nisi, non scelerisque lectus leo id neque. Quisque ultrices at risus sed porta. Suspendisse vel augue leo. Curabitur faucibus orci a libero semper rutrum vitae id ligula. Sed massa lorem, tempor id quam vel, pharetra tristique dolor. In pellentesque, libero quis tempor varius, libero mi mollis eros, scelerisque ullamcorper justo ipsum dignissim urna. Fusce dignissim urna et arcu aliquet, sit amet consectetur sem hendrerit. Aenean mattis ante nec odio varius, non convallis justo posuere. Vivamus ac sem non augue lacinia porttitor non ut nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi a leo et enim mattis ornare non non elit.', '2020-01-28 18:31:56'),
+(2, 2, 7, 'Pellentesque malesuada laoreet enim sed bibendum. Quisque lobortis sed justo nec posuere. Vivamus mattis sem tellus, a elementum tortor dignissim in. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel leo a mauris convallis semper. Nulla porttitor vel tortor non ultrices. Fusce eleifend nisi ac arcu finibus rhoncus. Ut nec enim ac quam scelerisque malesuada.', '2020-01-28 19:01:36'),
+(3, 2, 7, 'Vestibulum consequat turpis sed massa porta, non imperdiet sapien scelerisque. Nam vestibulum sit amet tortor nec facilisis. Sed pretium mi vitae ex viverra, in dignissim urna congue. Proin sem felis, accumsan nec interdum et, condimentum sit amet eros. Suspendisse sollicitudin et nisl at pharetra. Integer dignissim erat sapien, non pulvinar magna rhoncus eu. Morbi ultricies risus id neque consequat, dictum convallis arcu ornare. Proin iaculis mauris sit amet est faucibus, sit amet maximus purus gravida.', '2020-01-28 22:25:14'),
+(4, 3, 7, 'Ut consectetur feugiat hendrerit. Donec nisi ex, aliquet ut lectus a, bibendum vulputate libero. Cras eu purus ligula. Sed eleifend nisl vel tincidunt commodo. Nullam fermentum consectetur ante consectetur congue. Integer dolor tortor, dapibus eget maximus non, fringilla in sem. Phasellus volutpat elit et ante convallis cursus. Vestibulum feugiat ipsum a venenatis bibendum. Morbi interdum euismod fermentum. Mauris in elit eros. Vestibulum convallis libero vitae dui aliquet efficitur. Proin mauris ligula, commodo quis enim sed, convallis pellentesque arcu. Donec et nisi quis urna consectetur tincidunt eu non odio. Proin bibendum nisl sed fringilla iaculis. Quisque eu luctus dolor.', '2020-01-28 22:26:11');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `asi_rols`
 --
 
@@ -315,6 +366,12 @@ ALTER TABLE `asi_appointment`
   ADD PRIMARY KEY (`id_appointment`);
 
 --
+-- Indices de la tabla `asi_appointments_gallery`
+--
+ALTER TABLE `asi_appointments_gallery`
+  ADD PRIMARY KEY (`id_gallery`);
+
+--
 -- Indices de la tabla `asi_audit`
 --
 ALTER TABLE `asi_audit`
@@ -373,6 +430,12 @@ ALTER TABLE `asi_companies`
   ADD UNIQUE KEY `company_name` (`name_company`);
 
 --
+-- Indices de la tabla `asi_messages`
+--
+ALTER TABLE `asi_messages`
+  ADD PRIMARY KEY (`id_message`);
+
+--
 -- Indices de la tabla `asi_rols`
 --
 ALTER TABLE `asi_rols`
@@ -399,7 +462,13 @@ ALTER TABLE `asi_users_rols`
 -- AUTO_INCREMENT de la tabla `asi_appointment`
 --
 ALTER TABLE `asi_appointment`
-  MODIFY `id_appointment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_appointment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `asi_appointments_gallery`
+--
+ALTER TABLE `asi_appointments_gallery`
+  MODIFY `id_gallery` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `asi_audit`
@@ -454,6 +523,12 @@ ALTER TABLE `asi_audit_types_assets`
 --
 ALTER TABLE `asi_companies`
   MODIFY `id_company` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `asi_messages`
+--
+ALTER TABLE `asi_messages`
+  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `asi_rols`
