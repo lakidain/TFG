18612bc20819857edb9c9a68748d483a2f13c89f
Ander Lakidain de Arriba <lakidainander@gmail.com>
2020-02-10 12:41:57 +0100

    Update database_architecture.sql

--- Resources/database_architecture.sql
+++ Resources/database_architecture.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-01-2020 a las 14:14:13
+-- Tiempo de generación: 10-02-2020 a las 12:40:56
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.10
 
@@ -44,7 +44,7 @@ CREATE TABLE `asi_appointment` (
 --
 
 INSERT INTO `asi_appointment` (`id_appointment`, `id_audit`, `name_appointment`, `description_appointment`, `date_appointment`, `state_appointment`) VALUES
-(2, 1, 'Coches wapos', 'Amos a ver los cochecillos', '2020-01-28 23:50:00', 0),
+(2, 1, 'Coches wapos', 'Phasellus felis nisi, vehicula quis imperdiet ut, efficitur vitae lorem. Praesent aliquam eget lorem eu convallis. Fusce viverra ullamcorper sagittis. Quisque tincidunt ipsum ante, vitae venenatis dui dapibus ultricies. Pellentesque gravida ut nisi malesu', '2020-01-28 23:50:00', 0),
 (3, 1, 'Cableado Electrico', 'Nos encargaremos de hablar sobre el cableado electrico', '2020-01-31 14:00:00', 1);
 
 -- --------------------------------------------------------
@@ -56,21 +56,25 @@ INSERT INTO `asi_appointment` (`id_appointment`, `id_audit`, `name_appointment`,
 CREATE TABLE `asi_appointments_gallery` (
   `id_gallery` int(11) NOT NULL,
   `id_appointment` int(11) NOT NULL,
-  `photo_gallery` varchar(200) COLLATE utf8_spanish_ci NOT NULL
+  `photo_gallery` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `description_gallery` varchar(100) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `asi_appointments_gallery`
 --
 
-INSERT INTO `asi_appointments_gallery` (`id_gallery`, `id_appointment`, `photo_gallery`) VALUES
-(1, 2, '31247.jpg'),
-(2, 2, '47741453-189c-4f4f-a6cb-57eb291d152f_31247.jpg'),
-(3, 2, '4f7ba53a-ad70-4b02-9f54-aeb4d68dc65f_31247.jpg'),
-(4, 2, 'a08039d5-a6af-4273-9f66-b2fee0daa57b_31247.jpg'),
-(5, 2, '63fd0881-5adb-432f-bb01-9f2a13e25773_31247.jpg'),
-(6, 2, '5b43ee4e-94ca-4ddc-af39-8015b6ac2e1d_amazon-reveals-new-server-chip-to-take-on-intel-1280x720.jpg'),
-(7, 3, 'f14d88b6-c399-444b-9381-445dd46daced_Añadirunpocodetexto(4).png');
+INSERT INTO `asi_appointments_gallery` (`id_gallery`, `id_appointment`, `photo_gallery`, `description_gallery`) VALUES
+(1, 2, '31247.jpg', 'Hola'),
+(2, 2, '47741453-189c-4f4f-a6cb-57eb291d152f_31247.jpg', 'Saludos'),
+(3, 2, '4f7ba53a-ad70-4b02-9f54-aeb4d68dc65f_31247.jpg', 'Adios'),
+(4, 2, 'a08039d5-a6af-4273-9f66-b2fee0daa57b_31247.jpg', 'Buenos Dias'),
+(5, 2, '63fd0881-5adb-432f-bb01-9f2a13e25773_31247.jpg', 'Que tal'),
+(6, 2, '5b43ee4e-94ca-4ddc-af39-8015b6ac2e1d_amazon-reveals-new-server-chip-to-take-on-intel-1280x720.jpg', 'Buen invierno'),
+(7, 3, 'f14d88b6-c399-444b-9381-445dd46daced_Añadirunpocodetexto(4).png', 'Hasta luego'),
+(8, 2, 'b11039fc-e62a-4688-91ee-595593925b79_servicioscpd.jpg', 'undefined'),
+(9, 2, '78475be1-6767-44ce-bb85-38b1efd8d668_servicioscpd.jpg', 'Cableado'),
+(10, 2, 'ec0594c8-82ea-4b6b-8fae-af8cabc9cb7f_01-1170x832.jpg', 'Mas cables');
 
 -- --------------------------------------------------------
 
@@ -132,7 +136,7 @@ INSERT INTO `asi_auditors` (`id_auditor`, `name_auditor`, `dni_auditor`, `email_
 CREATE TABLE `asi_audit_answers` (
   `id_audit_answer` int(11) NOT NULL,
   `answer_audit_answer` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `score` int(11) NOT NULL
+  `score_audit_answer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -157,13 +161,33 @@ INSERT INTO `asi_audit_assets` (`id_audit_asset`, `name_audit_asset`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `asi_audit_assets_questions`
+-- Estructura de tabla para la tabla `asi_audit_assets_threats`
 --
 
-CREATE TABLE `asi_audit_assets_questions` (
-  `id_audit_asset_question` int(11) NOT NULL,
-  `id_asset` int(11) NOT NULL,
-  `id_question` int(11) NOT NULL
+CREATE TABLE `asi_audit_assets_threats` (
+  `id_audit_asset_threat` int(11) NOT NULL,
+  `id_audit_asset` int(11) NOT NULL,
+  `id_audit_threat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `asi_audit_assets_threats`
+--
+
+INSERT INTO `asi_audit_assets_threats` (`id_audit_asset_threat`, `id_audit_asset`, `id_audit_threat`) VALUES
+(1, 2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asi_audit_employees`
+--
+
+CREATE TABLE `asi_audit_employees` (
+  `id_audit_employees` int(11) NOT NULL,
+  `id_audit` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `appointment_permit_audit_employees` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -188,6 +212,24 @@ CREATE TABLE `asi_audit_questions_answers` (
   `id_audit_question` int(11) NOT NULL,
   `id_audit_answer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asi_audit_threats`
+--
+
+CREATE TABLE `asi_audit_threats` (
+  `id_audit_threat` int(11) NOT NULL,
+  `name_audit_threat` varchar(30) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `asi_audit_threats`
+--
+
+INSERT INTO `asi_audit_threats` (`id_audit_threat`, `name_audit_threat`) VALUES
+(3, 'Fuerza Bruta');
 
 -- --------------------------------------------------------
 
@@ -281,7 +323,8 @@ INSERT INTO `asi_messages` (`id_message`, `id_appointment`, `id_user`, `text_mes
 (1, 2, 7, 'Morbi dignissim nec odio nec dictum. Cras elementum, massa eget luctus viverra, erat ipsum rutrum nisi, non scelerisque lectus leo id neque. Quisque ultrices at risus sed porta. Suspendisse vel augue leo. Curabitur faucibus orci a libero semper rutrum vitae id ligula. Sed massa lorem, tempor id quam vel, pharetra tristique dolor. In pellentesque, libero quis tempor varius, libero mi mollis eros, scelerisque ullamcorper justo ipsum dignissim urna. Fusce dignissim urna et arcu aliquet, sit amet consectetur sem hendrerit. Aenean mattis ante nec odio varius, non convallis justo posuere. Vivamus ac sem non augue lacinia porttitor non ut nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi a leo et enim mattis ornare non non elit.', '2020-01-28 18:31:56'),
 (2, 2, 7, 'Pellentesque malesuada laoreet enim sed bibendum. Quisque lobortis sed justo nec posuere. Vivamus mattis sem tellus, a elementum tortor dignissim in. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel leo a mauris convallis semper. Nulla porttitor vel tortor non ultrices. Fusce eleifend nisi ac arcu finibus rhoncus. Ut nec enim ac quam scelerisque malesuada.', '2020-01-28 19:01:36'),
 (3, 2, 7, 'Vestibulum consequat turpis sed massa porta, non imperdiet sapien scelerisque. Nam vestibulum sit amet tortor nec facilisis. Sed pretium mi vitae ex viverra, in dignissim urna congue. Proin sem felis, accumsan nec interdum et, condimentum sit amet eros. Suspendisse sollicitudin et nisl at pharetra. Integer dignissim erat sapien, non pulvinar magna rhoncus eu. Morbi ultricies risus id neque consequat, dictum convallis arcu ornare. Proin iaculis mauris sit amet est faucibus, sit amet maximus purus gravida.', '2020-01-28 22:25:14'),
-(4, 3, 7, 'Ut consectetur feugiat hendrerit. Donec nisi ex, aliquet ut lectus a, bibendum vulputate libero. Cras eu purus ligula. Sed eleifend nisl vel tincidunt commodo. Nullam fermentum consectetur ante consectetur congue. Integer dolor tortor, dapibus eget maximus non, fringilla in sem. Phasellus volutpat elit et ante convallis cursus. Vestibulum feugiat ipsum a venenatis bibendum. Morbi interdum euismod fermentum. Mauris in elit eros. Vestibulum convallis libero vitae dui aliquet efficitur. Proin mauris ligula, commodo quis enim sed, convallis pellentesque arcu. Donec et nisi quis urna consectetur tincidunt eu non odio. Proin bibendum nisl sed fringilla iaculis. Quisque eu luctus dolor.', '2020-01-28 22:26:11');
+(4, 3, 7, 'Ut consectetur feugiat hendrerit. Donec nisi ex, aliquet ut lectus a, bibendum vulputate libero. Cras eu purus ligula. Sed eleifend nisl vel tincidunt commodo. Nullam fermentum consectetur ante consectetur congue. Integer dolor tortor, dapibus eget maximus non, fringilla in sem. Phasellus volutpat elit et ante convallis cursus. Vestibulum feugiat ipsum a venenatis bibendum. Morbi interdum euismod fermentum. Mauris in elit eros. Vestibulum convallis libero vitae dui aliquet efficitur. Proin mauris ligula, commodo quis enim sed, convallis pellentesque arcu. Donec et nisi quis urna consectetur tincidunt eu non odio. Proin bibendum nisl sed fringilla iaculis. Quisque eu luctus dolor.', '2020-01-28 22:26:11'),
+(5, 2, 7, 'Buenos dias', '2020-01-29 23:21:51');
 
 -- --------------------------------------------------------
 
@@ -392,10 +435,16 @@ ALTER TABLE `asi_audit_assets`
   ADD UNIQUE KEY `name_audit_asset` (`name_audit_asset`);
 
 --
--- Indices de la tabla `asi_audit_assets_questions`
+-- Indices de la tabla `asi_audit_assets_threats`
 --
-ALTER TABLE `asi_audit_assets_questions`
-  ADD PRIMARY KEY (`id_audit_asset_question`);
+ALTER TABLE `asi_audit_assets_threats`
+  ADD PRIMARY KEY (`id_audit_asset_threat`);
+
+--
+-- Indices de la tabla `asi_audit_employees`
+--
+ALTER TABLE `asi_audit_employees`
+  ADD PRIMARY KEY (`id_audit_employees`);
 
 --
 -- Indices de la tabla `asi_audit_questions`
@@ -408,6 +457,12 @@ ALTER TABLE `asi_audit_questions`
 --
 ALTER TABLE `asi_audit_questions_answers`
   ADD PRIMARY KEY (`id_audit_question_answer`);
+
+--
+-- Indices de la tabla `asi_audit_threats`
+--
+ALTER TABLE `asi_audit_threats`
+  ADD PRIMARY KEY (`id_audit_threat`);
 
 --
 -- Indices de la tabla `asi_audit_types`
@@ -468,7 +523,7 @@ ALTER TABLE `asi_appointment`
 -- AUTO_INCREMENT de la tabla `asi_appointments_gallery`
 --
 ALTER TABLE `asi_appointments_gallery`
-  MODIFY `id_gallery` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_gallery` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `asi_audit`
@@ -489,10 +544,16 @@ ALTER TABLE `asi_audit_assets`
   MODIFY `id_audit_asset` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de la tabla `asi_audit_assets_questions`
+-- AUTO_INCREMENT de la tabla `asi_audit_assets_threats`
 --
-ALTER TABLE `asi_audit_assets_questions`
-  MODIFY `id_audit_asset_question` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `asi_audit_assets_threats`
+  MODIFY `id_audit_asset_threat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `asi_audit_employees`
+--
+ALTER TABLE `asi_audit_employees`
+  MODIFY `id_audit_employees` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `asi_audit_questions`
@@ -505,6 +566,12 @@ ALTER TABLE `asi_audit_questions`
 --
 ALTER TABLE `asi_audit_questions_answers`
   MODIFY `id_audit_question_answer` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `asi_audit_threats`
+--
+ALTER TABLE `asi_audit_threats`
+  MODIFY `id_audit_threat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `asi_audit_types`
@@ -528,7 +595,7 @@ ALTER TABLE `asi_companies`
 -- AUTO_INCREMENT de la tabla `asi_messages`
 --
 ALTER TABLE `asi_messages`
-  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `asi_rols`
